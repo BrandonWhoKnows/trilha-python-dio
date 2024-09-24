9710c896f3f91a1ab23088030f0ca7b21e0d8209
Brandon Serafim <br.serafim@gmail.com>
2024-09-24 19:36:35 -0300

    "Add comments"

--- 01 - Estrutura de dados/01 - Listas/13_pop.py
+++ 01 - Estrutura de dados/01 - Listas/13_pop.py
@@ -1,6 +1,10 @@
 linguagens = ["python", "js", "c", "java", "csharp"]
 
+# Retorna o último elemento da lista, "me diz qual o prato de cima que foi retirado"
+
 print(linguagens.pop())  # csharp
 print(linguagens.pop())  # java
 print(linguagens.pop())  # c
+
+# Ao passar o índice, ele retira o item correspondente
 print(linguagens.pop(0))  # python
