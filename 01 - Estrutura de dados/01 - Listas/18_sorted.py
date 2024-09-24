8131872aedeb3b289564f3a196788c048a992b26
Brandon Serafim <br.serafim@gmail.com>
2024-09-24 20:06:06 -0300

    "Add comment"

--- 01 - Estrutura de dados/01 - Listas/18_sorted.py
+++ 01 - Estrutura de dados/01 - Listas/18_sorted.py
@@ -1,4 +1,5 @@
 linguagens = ["python", "js", "c", "java", "csharp"]
 
+# função built-in do python que é equivalente ao sort
 print(sorted(linguagens, key=lambda x: len(x)))  # ["c", "js", "java", "python", "csharp"]
 print(sorted(linguagens, key=lambda x: len(x), reverse=True))  # ["python", "csharp", "java", "js", "c"]
