1210a45b2aad3c6f57cd7b70ab1d0c3f5cbc622e
Brandon Serafim <br.serafim@gmail.com>
2024-09-24 18:33:13 -0300

    "Add comment to explain index"

--- 01 - Estrutura de dados/01 - Listas/12_index.py
+++ 01 - Estrutura de dados/01 - Listas/12_index.py
@@ -1,4 +1,6 @@
 linguagens = ["python", "js", "c", "java", "csharp"]
 
+# Index avalia a primeira ocorrência de um item e retorna o número do índice
+
 print(linguagens.index("java"))  # 3
 print(linguagens.index("python"))  # 0
