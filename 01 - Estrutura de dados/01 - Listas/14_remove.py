ca9df5c146a7b528960532ef1e8614422ed3ca2b
Brandon Serafim <br.serafim@gmail.com>
2024-09-24 19:53:12 -0300

    "Add comments"

--- 01 - Estrutura de dados/01 - Listas/14_remove.py
+++ 01 - Estrutura de dados/01 - Listas/14_remove.py
@@ -1,5 +1,7 @@
-linguagens = ["python", "js", "c", "java", "csharp"]
+linguagens = ["python", "js", "c", "java", "csharp", "c"]
+
+# Retira apenas a primeira ocorrência do item especificado, se houver mais de um, ele retira a primeira ocorrência
 
 linguagens.remove("c")
 
-print(linguagens)  # ["python", "js", "java", "csharp"]
+print(linguagens)  # ["python", "js", "java", "csharp", "c"]
